0cc00038a98dc81dee7b59661c6fcd065ec77524
raphaelhla <raphael.agra@ccc.ufcg.edu.br>
2023-08-27 01:55:09 -0300

    Correção de erros no deploy.

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-export const apiUrl = 'http://localhost:8080';
-// export const apiUrl = 'https://health-notes-47645d4f2894.herokuapp.com';
+// export const apiUrl = 'http://localhost:8080';
+export const apiUrl = 'https://health-notes-47645d4f2894.herokuapp.com';
